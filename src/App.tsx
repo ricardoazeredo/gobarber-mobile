d2f491ce496c6b927d4187dec99691f0c225b623
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2021-02-14 15:37:11 -0300

    Updating Status Bar and background color

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
 
 const App: React.FC = () => (
-  <View>
-    <Text>Hello World</Text>
-  </View>
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }} />
+  </>
 );
 
 export default App;
