931c5d4c2f6a8a941522e0eb4fe513575adcb582
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2021-02-14 22:34:28 -0300

    Adding input fields and button

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,15 +1,16 @@
-import styled from "styled-components/native";
+import styled from 'styled-components/native';
 
 // eslint-disable-next-line import/prefer-default-export
 export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+  padding: 0 30px;
 `;
 
 export const Title = styled.Text`
   font-size: 24px;
   color: #f4ede8;
-  font-family: "RobotoSlab-Medium";
+  font-family: 'RobotoSlab-Medium';
   margin: 64px 0 24px;
 `;
